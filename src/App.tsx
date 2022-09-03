8fb2c7ed0d572e69b23dc0f2ae3f9b30cbb4c3fa
agustinagarciarey <agus.garciarey@gmail.com>
2022-09-03 03:33:46 -0300

    fix: imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import './App.css';
 import Cabecera from './components/Cabecera';
 import Listado from './components/Listado';
